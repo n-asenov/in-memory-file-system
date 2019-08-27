8621c1c7cf32b899a1994bc45ac66612f1160423
Nikolay Asenov <asenov.nikolay98@gmail.com>
2019-08-27 21:31:37 +0300

    Implement List Directory command

--- src/fileSystem/commands/ListDirectoryContent.java
+++ src/fileSystem/commands/ListDirectoryContent.java
@@ -2,12 +2,34 @@ package fileSystem.commands;
 
 import java.util.List;
 
+import fileSystem.fs.FileSystem;
+import fileSystem.fs.FilterBy;
+
 public class ListDirectoryContent implements Command {
+	private FileSystem fs;
 
-	@Override
-	public void execute(List<String> options, List<String> arguments) {
-		// TODO Auto-generated method stub
-
+	public ListDirectoryContent(FileSystem fs) {
+		this.fs = fs;
 	}
 
+	@Override
+	public String execute(List<String> options, List<String> arguments) {
+		FilterBy flag = FilterBy.DEFAULT;
+
+		for (String option : options) {
+			if(!option.equals("sortedDesc")) {
+				return "Invalid option!";
+			}
+			flag = FilterBy.SIZE_DESCENDING;
+		}
+		
+		StringBuilder result = new StringBuilder();
+
+		for (String directory : arguments) {
+			result.append(fs.getDirectoryContent(directory, flag));
+			result.append(System.lineSeparator());
+		}
+
+		return result.toString();
+	}
 }
---
+++ test/fileSystem/commands/ListDirectoryContentTest.java
@@ -0,0 +1,69 @@
+package fileSystem.commands;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import fileSystem.fs.FileSystem;
+
+public class ListDirectoryContentTest {
+
+	@Test
+	public void execute_CommandWithInvalidOptions_ReturnInvalidOptionMessage() {
+		ListDirectoryContent ls = new ListDirectoryContent(new FileSystem());
+
+		List<String> options = new ArrayList<String>();
+		options.add("al");
+		List<String> arguments = new ArrayList<String>();
+		arguments.add("/home");
+
+		String result = ls.execute(options, arguments);
+		String expectedResult = "Invalid option!";
+
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void execute_listEmptyDirectory_ReturnEmptyArray() {
+		ListDirectoryContent ls = new ListDirectoryContent(new FileSystem());
+
+		List<String> options = new ArrayList<String>();
+		List<String> arguments = new ArrayList<String>();
+		arguments.add("/home");
+		
+		assertEquals("" + System.lineSeparator(), ls.execute(options, arguments));
+	}
+	
+	@Test
+	public void execute_listTwoDirectories_ReturnContetnOfDirectories() {
+		ListDirectoryContent ls = new ListDirectoryContent(new FileSystem());
+
+		List<String> options = new ArrayList<String>();
+		List<String> arguments = new ArrayList<String>();
+		arguments.add("/");
+		arguments.add("/home");
+		
+		String result = ls.execute(options, arguments);
+		String expectedResult = "home " + System.lineSeparator() + System.lineSeparator();
+		
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void execute_listDirectoryWithSortedDescOption_ReturnContentSortedDesc() {
+		ListDirectoryContent ls = new ListDirectoryContent(new FileSystem());
+
+		List<String> options = new ArrayList<String>();
+		options.add("sortedDesc");
+		List<String> arguments = new ArrayList<String>();
+		arguments.add("/");
+		
+		String result = ls.execute(options, arguments);
+		String expectedResult = "home " + System.lineSeparator();
+		
+		assertEquals(expectedResult, result);
+	}
+}
