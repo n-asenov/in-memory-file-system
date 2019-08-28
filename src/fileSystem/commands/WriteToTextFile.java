893c9ab87fd0fd6396c6e82a15760ee638bcca44
Nikolay Asenov <asenov.nikolay98@gmail.com>
2019-08-28 11:32:18 +0300

    Implement write command

---
+++ src/fileSystem/commands/WriteToTextFile.java
@@ -0,0 +1,45 @@
+package fileSystem.commands;
+
+import java.util.List;
+
+import fileSystem.fs.FileSystem;
+
+public class WriteToTextFile implements Command {
+	private FileSystem fs;
+
+	public WriteToTextFile(FileSystem fs) {
+		this.fs = fs;
+	}
+
+	@Override
+	public String execute(List<String> options, List<String> arguments) {
+		boolean overwrite = false;
+
+		for (String option : options) {
+			if (!option.equals("overwrite")) {
+				return "Invalid option!";
+			}
+			overwrite = true;
+		}
+
+		if (arguments.size() != 3) {
+			return "Invalid number of arguments!";
+		}
+
+		if (!isNumber(arguments.get(1))) {
+			return "Second argument must be a positive number!";
+		}
+
+		String absolutePath = arguments.get(0);
+		int line = Integer.parseInt(arguments.get(1));
+		String lineContent = arguments.get(2);
+
+		String result = fs.writeToTextFile(absolutePath, line, lineContent, overwrite);
+
+		return result;
+	}
+
+	private boolean isNumber(String number) {
+		return number.matches("\\d*");
+	}
+}
---
+++ test/fileSystem/commands/WriteToTextFileTest.java
@@ -0,0 +1,97 @@
+package fileSystem.commands;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import fileSystem.fs.FileSystem;
+
+public class WriteToTextFileTest {
+
+	@Test
+	public void execute_CommandWithInvalidOption_ReturnInvalidOptionMessage() {
+		WriteToTextFile command = new WriteToTextFile(new FileSystem());
+
+		List<String> options = new ArrayList<String>();
+		options.add("append");
+		List<String> arguments = new ArrayList<String>();
+
+		String result = command.execute(options, arguments);
+		String expectedResult = "Invalid option!";
+
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void execute_CommandWithInvalidNumberOfArguments_ReturnInvalidNumberOfArgumentsMessage() {
+		WriteToTextFile command = new WriteToTextFile(new FileSystem());
+
+		List<String> options = new ArrayList<String>();
+		List<String> arguments = new ArrayList<String>();
+		arguments.add("/home/f1");
+
+		String result = command.execute(options, arguments);
+		String expectedResult = "Invalid number of arguments!";
+
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void execute_CommnadWithWrongSecondArgument_ReturnSecondArgumentErrorMessage() {
+		WriteToTextFile command = new WriteToTextFile(new FileSystem());
+
+		List<String> options = new ArrayList<String>();
+		List<String> arguments = new ArrayList<String>();
+		arguments.add("/home/f1");
+		arguments.add("Number");
+		arguments.add("Hello, World!");
+
+		String result = command.execute(options, arguments);
+		String expectedResult = "Second argument must be a positive number!";
+
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test 
+	public void execute_ValidCommandWithNoOption_WriteContentToTextFile() {
+		FileSystem  fs = new FileSystem();
+		fs.createTextFile("/home/f1");
+		
+		WriteToTextFile command = new WriteToTextFile(fs);
+		
+		List<String> options = new ArrayList<String>();
+		List<String> arguments = new ArrayList<String>();
+		arguments.add("/home/f1");
+		arguments.add("2");
+		arguments.add("Hello, World!");
+		
+		String result = command.execute(options, arguments);
+		String expectedResult = null;
+		
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void execute_ValidCommandWithOverwriteOption_WriteContentToTextFile() {
+		FileSystem  fs = new FileSystem();
+		fs.createTextFile("/home/f1");
+		
+		WriteToTextFile command = new WriteToTextFile(fs);
+		
+		List<String> options = new ArrayList<String>();
+		options.add("overwrite");
+		List<String> arguments = new ArrayList<String>();
+		arguments.add("/home/f1");
+		arguments.add("2");
+		arguments.add("Hello, World!");
+		
+		String result = command.execute(options, arguments);
+		String expectedResult = null;
+		
+		assertEquals(expectedResult, result);
+	}
+
+}
