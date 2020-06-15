2b2ca6d9ca2ecffc80c19aa89086731832e8a88e
Nikolay Asenov <asenov.nikolay98@gmail.com>
2020-06-15 15:56:10 +0300

    refactor RemoveTextFileTest class

--- test/commands/RemoveTextFileTest.java
+++ test/commands/RemoveTextFileTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertArrayEquals;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
@@ -20,61 +21,70 @@ import path.Path;
 
 public class RemoveTextFileTest {
     private static final Comparator<File> DEFAULT = (f1, f2) -> f1.getName().compareTo(f2.getName());
-    
-    private RemoveTextFile command;
+
     private VirtualFileSystem fileSystem;
-    private Set<String> options;
+    private RemoveTextFile command;
     private List<String> arguments;
+    private Set<String> options;
 
     @Before
     public void init() {
 	fileSystem = new VirtualFileSystem();
 	command = new RemoveTextFile(fileSystem, new Path());
+	arguments = new ArrayList<>();
 	options = new HashSet<>();
-	arguments = new ArrayList<String>();
     }
 
     @Test(expected = InvalidArgumentException.class)
-    public void execute_InvalidOption_ThrowInvalidArgumentException() throws InvalidArgumentException, IOException {
+    public void execute_InvalidOption_ThrowInvalidArgumentException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	options.add("-invalid");
 
 	command.execute(arguments, options);
     }
 
     @Test(expected = InvalidArgumentException.class)
-    public void execute_WrongPathToFile_ThrowInvalidArgumentException() throws InvalidArgumentException, IOException {
+    public void execute_WrongPathToFile_ThrowInvalidArgumentException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("/home/dir1/f1");
 
 	command.execute(arguments, options);
     }
 
     @Test(expected = FileNotFoundException.class)
-    public void execute_TextFileDoesNotExists_ThrowFileNotFoudException() throws InvalidArgumentException, IOException {
+    public void execute_TextFileDoesNotExists_ThrowFileNotFoudException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("/home/f1");
 
 	command.execute(arguments, options);
     }
 
     @Test(expected = FileNotFoundException.class)
-    public void execute_FileIsDirectory_ThrowFileNotFoundException() throws InvalidArgumentException, IOException {
+    public void execute_FileIsDirectory_ThrowFileNotFoundException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("/home");
 
 	command.execute(arguments, options);
     }
 
     @Test
-    public void execute_TextFile_RemoveTextFileFromDirectory() throws InvalidArgumentException, IOException {
-	fileSystem.createTextFile("/home/f1");
-	arguments.add("/home/f1");
+    public void execute_TextFile_RemoveTextFileFromDirectory()
+	    throws InvalidArgumentException, FileAlreadyExistsException, FileNotFoundException {
+	String textFileAbsolutePath = "/home/f1";
+	fileSystem.createTextFile(textFileAbsolutePath);
+	arguments.add(textFileAbsolutePath);
+
+	command.execute(arguments, options);
 
 	String[] expectedResult = {};
-	command.execute(arguments, options);
-	
-	assertArrayEquals(expectedResult, fileSystem.getDirectoryContent("/home", DEFAULT).toArray());
+	List<String> directoryContent = fileSystem.getDirectoryContent("/home", DEFAULT);
+	String[] actualResult = directoryContent.toArray(new String[0]);
+	assertArrayEquals(expectedResult, actualResult);
     }
 
     @Test
-    public void execute_SeveralTextFiles_RemoveAllTextFiles() throws InvalidArgumentException, IOException {
+    public void execute_SeveralTextFiles_RemoveAllTextFiles()
+	    throws InvalidArgumentException, FileAlreadyExistsException, FileNotFoundException {
 	int numberOfFiles = 10;
 
 	for (int i = 0; i < numberOfFiles; i++) {
@@ -82,9 +92,12 @@ public class RemoveTextFileTest {
 	    fileSystem.createTextFile(fileName);
 	    arguments.add(fileName);
 	}
-	String[] expectedResult = {};
+
 	command.execute(arguments, options);
 	
-	assertArrayEquals(expectedResult, fileSystem.getDirectoryContent("/home", DEFAULT).toArray());
+	String[] expectedResult = {};
+	List<String> directoryContent = fileSystem.getDirectoryContent("/home", DEFAULT);
+	String[] actualResult = directoryContent.toArray(new String[0]);
+	assertArrayEquals(expectedResult, actualResult);
     }
 }
