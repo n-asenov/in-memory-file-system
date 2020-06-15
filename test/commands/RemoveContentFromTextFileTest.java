39b365b71031e6dfbdc53afaeea6469d3d4c5649
Nikolay Asenov <asenov.nikolay98@gmail.com>
2020-06-15 15:51:12 +0300

    refactor RemoveContentFromTextFileTest class

--- test/commands/RemoveContentFromTextFileTest.java
+++ test/commands/RemoveContentFromTextFileTest.java
@@ -3,7 +3,7 @@ package commands;
 import static org.junit.Assert.assertEquals;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -18,28 +18,30 @@ import filesystem.exceptions.NotEnoughMemoryException;
 import path.Path;
 
 public class RemoveContentFromTextFileTest {
-    private RemoveContentFromTextFile command;
     private VirtualFileSystem fileSystem;
-    private Set<String> options;
+    private RemoveContentFromTextFile command;
     private List<String> arguments;
+    private Set<String> options;
 
     @Before
     public void init() {
 	fileSystem = new VirtualFileSystem();
 	command = new RemoveContentFromTextFile(fileSystem, new Path());
+	arguments = new ArrayList<>();
 	options = new HashSet<>();
-	arguments = new ArrayList<String>();
     }
 
     @Test(expected = InvalidArgumentException.class)
-    public void execute_InvalidOption_ThrowInvalidArgumentException() throws InvalidArgumentException, IOException {
+    public void execute_InvalidOption_ThrowInvalidArgumentException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	options.add("-invalid");
 
 	command.execute(arguments, options);
     }
 
     @Test(expected = InvalidArgumentException.class)
-    public void execute_TooMuchArguments_ThrowInvalidArgumentException() throws InvalidArgumentException, IOException {
+    public void execute_TooMuchArguments_ThrowInvalidArgumentException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("f1");
 	arguments.add("1-5");
 	arguments.add("6-10");
@@ -49,12 +51,13 @@ public class RemoveContentFromTextFileTest {
 
     @Test(expected = InvalidArgumentException.class)
     public void execute_NotEnoughArguments_ThrowInvalidArgumentException()
-	    throws InvalidArgumentException, IOException {
+	    throws InvalidArgumentException, FileNotFoundException {
 	command.execute(arguments, options);
     }
 
     @Test(expected = InvalidArgumentException.class)
-    public void execute_InvalidInterval_ThrowInvalidArgumentException() throws InvalidArgumentException, IOException {
+    public void execute_InvalidInterval_ThrowInvalidArgumentException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("f1");
 	arguments.add("1-5-10");
 
@@ -63,7 +66,7 @@ public class RemoveContentFromTextFileTest {
 
     @Test(expected = InvalidArgumentException.class)
     public void execute_WrongPathToTextFile_ThrowInvalidArgumentExcepiton()
-	    throws InvalidArgumentException, IOException {
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("/home/dir1/f1");
 	arguments.add("1-5");
 
@@ -72,7 +75,7 @@ public class RemoveContentFromTextFileTest {
 
     @Test(expected = FileNotFoundException.class)
     public void execute_TextFileDoesNotExists_ThrowFileNotFoundException()
-	    throws InvalidArgumentException, IOException {
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("/home/f1");
 	arguments.add("1-5");
 
@@ -80,7 +83,8 @@ public class RemoveContentFromTextFileTest {
     }
 
     @Test(expected = FileNotFoundException.class)
-    public void execute_FileIsDirectory_ThrowFileNotFoundException() throws InvalidArgumentException, IOException {
+    public void execute_FileIsDirectory_ThrowFileNotFoundException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("/home");
 	arguments.add("1-5");
 
@@ -88,7 +92,8 @@ public class RemoveContentFromTextFileTest {
     }
 
     @Test(expected = InvalidArgumentException.class)
-    public void execute_WrongInterval_ThrowInvalidArgumentException() throws InvalidArgumentException, IOException {
+    public void execute_WrongInterval_ThrowInvalidArgumentException()
+	    throws InvalidArgumentException, FileNotFoundException {
 	arguments.add("f1");
 	arguments.add("asdas-asdasd");
 
@@ -96,8 +101,8 @@ public class RemoveContentFromTextFileTest {
     }
 
     @Test
-    public void execute_TextFile_ClearTextFileContent()
-	    throws InvalidArgumentException, NotEnoughMemoryException, IOException {
+    public void execute_TextFile_ClearTextFileContent() throws InvalidArgumentException, NotEnoughMemoryException,
+	    FileAlreadyExistsException, FileNotFoundException {
 	String absolutePath = "/home/f1";
 	fileSystem.createTextFile(absolutePath);
 
