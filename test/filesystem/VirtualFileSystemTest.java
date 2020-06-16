f78bb3c27383a1eb42af6a3850fc9cc11f3e6c98
Nikolay Asenov <asenov.nikolay98@gmail.com>
2020-06-16 09:06:56 +0300

    refactor VirtualFileSystemTest class

--- test/filesystem/VirtualFileSystemTest.java
+++ test/filesystem/VirtualFileSystemTest.java
@@ -10,6 +10,7 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.InvalidPathException;
 import java.nio.file.NotDirectoryException;
 import java.util.Comparator;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,46 +20,53 @@ import filesystem.exceptions.NotEnoughMemoryException;
 
 public class VirtualFileSystemTest {
     private static final Comparator<File> DEFAULT = (f1, f2) -> f1.getName().compareTo(f2.getName());
-    
-    private VirtualFileSystem fs;
+
+    private VirtualFileSystem fileSystem;
 
     @Before
     public void init() {
-	fs = new VirtualFileSystem();
+	fileSystem = new VirtualFileSystem();
     }
 
     @Test
     public void makeDirectory_NewDirectory_DirectoryAddedToFileSystem()
-	    throws FileAlreadyExistsException, NotDirectoryException, FileNotFoundException, InvalidArgumentException {
-	fs.makeDirectory("/home/newDir");
+	    throws FileAlreadyExistsException, FileNotFoundException, InvalidArgumentException {
+	fileSystem.makeDirectory("/home/newDir");
+
 	String[] expectedResult = { "newDir" };
-	assertArrayEquals(expectedResult, fs.getDirectoryContent("/home", DEFAULT).toArray());
+	assertArrayEquals(expectedResult, getActualDirectoryContent("/home"));
+    }
+
+    private String[] getActualDirectoryContent(String directoryAbsolutePath)
+	    throws FileNotFoundException, InvalidArgumentException {
+	List<String> directoryContent = fileSystem.getDirectoryContent(directoryAbsolutePath, DEFAULT);
+	return directoryContent.toArray(new String[0]);
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void makeDirectory_PathDoesNotExists_ThrowInvalidPathException()
 	    throws FileAlreadyExistsException, InvalidArgumentException {
-	fs.makeDirectory("/wrongPath/dir1");
+	fileSystem.makeDirectory("/wrongPath/dir1");
     }
 
     @Test(expected = FileAlreadyExistsException.class)
     public void makeDirectory_AddExistingDirectory_ThrowFileAlreadyExistsException()
 	    throws FileAlreadyExistsException, InvalidArgumentException {
-	fs.makeDirectory("/");
+	fileSystem.makeDirectory("/");
     }
 
     @Test
     public void createTextFile_NewTextFile_TextFileAddedInDirectory()
-	    throws FileAlreadyExistsException, NotDirectoryException, FileNotFoundException, InvalidArgumentException {
-	fs.createTextFile("/home/f1");
+	    throws FileAlreadyExistsException, FileNotFoundException, InvalidArgumentException {
+	fileSystem.createTextFile("/home/f1");
 	String[] expectedResult = { "f1" };
-	assertArrayEquals(expectedResult, fs.getDirectoryContent("/home", DEFAULT).toArray());
+	assertArrayEquals(expectedResult, getActualDirectoryContent("/home"));
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void createTextFile_PathDoesNotExists_ThrowInvalidPathException()
 	    throws FileAlreadyExistsException, InvalidArgumentException {
-	fs.createTextFile("/wrongPath/f1");
+	fileSystem.createTextFile("/wrongPath/f1");
     }
 
     @Test(expected = FileAlreadyExistsException.class)
@@ -66,8 +74,8 @@ public class VirtualFileSystemTest {
 	    throws FileAlreadyExistsException, InvalidArgumentException {
 	String absolutePath = "/home/f1";
 
-	fs.createTextFile(absolutePath);
-	fs.createTextFile(absolutePath);
+	fileSystem.createTextFile(absolutePath);
+	fileSystem.createTextFile(absolutePath);
     }
 
     @Test
@@ -75,39 +83,39 @@ public class VirtualFileSystemTest {
 	    throws FileAlreadyExistsException, FileNotFoundException, InvalidArgumentException {
 	String absolutePath = "/home/f1";
 
-	fs.createTextFile(absolutePath);
+	fileSystem.createTextFile(absolutePath);
 
-	assertEquals("", fs.getTextFileContent(absolutePath));
+	assertEquals("", fileSystem.getTextFileContent(absolutePath));
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void getTextFileContent_PathDoesNotExists_ThrowInvalidPathException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.getTextFileContent("/home/dir1/f1");
+	fileSystem.getTextFileContent("/home/dir1/f1");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void getTextFileContent_FileDoesNotExists_ThrowFileNotFoundException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.getTextFileContent("/home/f1");
+	fileSystem.getTextFileContent("/home/f1");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void getTextFileContent_FileIsDirectory_ThrowFileNotFoundException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.getTextFileContent("/home");
+	fileSystem.getTextFileContent("/home");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void writeToTextFile_WriteToNonExistingFile_ThrowFileNotFoundException()
 	    throws FileNotFoundException, NotEnoughMemoryException, InvalidArgumentException {
-	fs.writeToTextFile("/home/f1", 1, "Test");
+	fileSystem.writeToTextFile("/home/f1", 1, "Test");
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void writeToTextFile_PathToFileDoesNotExists_ThrowInvalidPathException()
 	    throws FileNotFoundException, NotEnoughMemoryException, InvalidArgumentException {
-	fs.writeToTextFile("/home/dir1/f1", 1, "Test");
+	fileSystem.writeToTextFile("/home/dir1/f1", 1, "Test");
     }
 
     @Test
@@ -115,18 +123,17 @@ public class VirtualFileSystemTest {
 	    FileAlreadyExistsException, NotEnoughMemoryException, InvalidArgumentException {
 	String absolutePath = "/home/f1";
 
-	fs.createTextFile(absolutePath);
+	fileSystem.createTextFile(absolutePath);
+	fileSystem.writeToTextFile(absolutePath, 1, "Test");
 
-	fs.writeToTextFile(absolutePath, 1, "Test");
-
-	assertEquals("Test", fs.getTextFileContent(absolutePath));
-	assertEquals(5, fs.getUsedMemory());
+	assertEquals("Test", fileSystem.getTextFileContent(absolutePath));
+	assertEquals(5, fileSystem.getUsedMemory());
     }
 
     @Test(expected = FileNotFoundException.class)
     public void writeToTextFile_FileIsDirectory_ThrowFileNotFoundException()
 	    throws FileNotFoundException, NotEnoughMemoryException, InvalidArgumentException {
-	fs.writeToTextFile("/home", 1, "hello");
+	fileSystem.writeToTextFile("/home", 1, "hello");
     }
 
     @Test(expected = NotEnoughMemoryException.class)
@@ -134,8 +141,7 @@ public class VirtualFileSystemTest {
 	    throws FileAlreadyExistsException, FileNotFoundException, NotEnoughMemoryException,
 	    InvalidArgumentException {
 	String absolutePath = "/home/f1";
-	fs.createTextFile(absolutePath);
-	assertEquals(0, fs.getUsedMemory());
+	fileSystem.createTextFile(absolutePath);
 
 	StringBuilder content = new StringBuilder();
 
@@ -143,7 +149,7 @@ public class VirtualFileSystemTest {
 	    content.append('a');
 	}
 
-	fs.writeToTextFile(absolutePath, 1, content.toString());
+	fileSystem.writeToTextFile(absolutePath, 1, content.toString());
     }
 
     @Test
@@ -151,12 +157,12 @@ public class VirtualFileSystemTest {
 	    FileNotFoundException, NotEnoughMemoryException, InvalidArgumentException {
 	String absolutePath = "/home/f1";
 
-	fs.createTextFile(absolutePath);
-	fs.writeToTextFile(absolutePath, 1, "Hello");
-	assertEquals(6, fs.getUsedMemory());
+	fileSystem.createTextFile(absolutePath);
+	fileSystem.writeToTextFile(absolutePath, 1, "Hello");
+	assertEquals(6, fileSystem.getUsedMemory());
 
-	fs.writeToTextFile(absolutePath, 1, "A");
-	assertEquals(2, fs.getUsedMemory());
+	fileSystem.writeToTextFile(absolutePath, 1, "A");
+	assertEquals(2, fileSystem.getUsedMemory());
     }
 
     @Test
@@ -164,20 +170,20 @@ public class VirtualFileSystemTest {
 	    FileNotFoundException, NotEnoughMemoryException, InvalidArgumentException {
 	String absolutePath = "/home/f1";
 
-	fs.createTextFile(absolutePath);
-	fs.writeToTextFile(absolutePath, 1, "hello");
-	assertEquals(6, fs.getUsedMemory());
-	fs.appendToTextFile(absolutePath, 1, " world");
-	assertEquals(12, fs.getUsedMemory());
+	fileSystem.createTextFile(absolutePath);
+	fileSystem.writeToTextFile(absolutePath, 1, "hello");
+	assertEquals(6, fileSystem.getUsedMemory());
+	fileSystem.appendToTextFile(absolutePath, 1, " world");
+	assertEquals(12, fileSystem.getUsedMemory());
     }
 
     @Test
     public void writeToTextFile_FreeEnoughSpaceByDeletingFilesToDelete_ContentWrittenToTextFile()
 	    throws FileAlreadyExistsException, FileNotFoundException, NotEnoughMemoryException,
 	    InvalidArgumentException {
-	fs.createTextFile("/home/f1");
-	fs.writeToTextFile("/home/f1", 1, "Hello");
-	fs.removeTextFile("/home/f1");
+	fileSystem.createTextFile("/home/f1");
+	fileSystem.writeToTextFile("/home/f1", 1, "Hello");
+	fileSystem.removeTextFile("/home/f1");
 
 	StringBuilder content = new StringBuilder();
 
@@ -185,59 +191,59 @@ public class VirtualFileSystemTest {
 	    content.append('a');
 	}
 
-	fs.createTextFile("/home/f2");
-	fs.writeToTextFile("/home/f2", 1, content.toString());
+	fileSystem.createTextFile("/home/f2");
+	fileSystem.writeToTextFile("/home/f2", 1, content.toString());
 
-	assertEquals(content.toString(), fs.getTextFileContent("/home/f2"));
-	assertEquals(VirtualFileSystem.MEMORY_CAPACITY, fs.getUsedMemory());
+	assertEquals(content.toString(), fileSystem.getTextFileContent("/home/f2"));
+	assertEquals(VirtualFileSystem.MEMORY_CAPACITY, fileSystem.getUsedMemory());
     }
 
     @Test(expected = FileNotFoundException.class)
     public void getDirectoryContent_NonExistingDirectory_ThrowFileNotFoundException()
 	    throws NotDirectoryException, FileNotFoundException, InvalidArgumentException {
-	fs.getDirectoryContent("/home/dir1", DEFAULT);
+	fileSystem.getDirectoryContent("/home/dir1", DEFAULT);
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void getDirectoryContent_PathDoesNotExists_ThrowInvalidPathException()
-	    throws InvalidPathException, NotDirectoryException, FileNotFoundException, InvalidArgumentException {
-	fs.getDirectoryContent("/home/dir1/dir2/", DEFAULT);
+	    throws InvalidPathException, FileNotFoundException, InvalidArgumentException {
+	fileSystem.getDirectoryContent("/home/dir1/dir2/", DEFAULT);
     }
 
     @Test
     public void getDirectoryContent_EmptyDirectory_ReturnEmptyString()
-	    throws NotDirectoryException, FileNotFoundException, InvalidArgumentException {
+	    throws FileNotFoundException, InvalidArgumentException {
 	String[] expectedResult = {};
-	assertArrayEquals(expectedResult, fs.getDirectoryContent("/home", DEFAULT).toArray());
+	assertArrayEquals(expectedResult, getActualDirectoryContent("/home"));
     }
 
     @Test
     public void getDirectoryContent_RootDirectory_ReturnRootContent()
 	    throws NotDirectoryException, FileNotFoundException, InvalidArgumentException {
 	String[] expectedResult = { "home" };
-	assertArrayEquals(expectedResult, fs.getDirectoryContent("/", DEFAULT).toArray());
+	assertArrayEquals(expectedResult, getActualDirectoryContent("/"));
     }
 
     @Test
     public void isDirectory_ExistingDirectory_True() throws FileNotFoundException, InvalidArgumentException {
-	assertTrue(fs.isDirectory("/home"));
+	assertTrue(fileSystem.isDirectory("/home"));
     }
 
     @Test
     public void isDirectory_Root_True() throws FileNotFoundException, InvalidArgumentException {
-	assertTrue(fs.isDirectory("/"));
+	assertTrue(fileSystem.isDirectory("/"));
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void isDirectory_DirectoryWithWrongPath_ThrowInvalidPathException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.isDirectory("/home/dir1/dir2");
+	fileSystem.isDirectory("/home/dir1/dir2");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void isDirectory_NonExistentDirectory_ThrowFileNotFoundException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	assertEquals("File doesn't exists!", fs.isDirectory("/home/dir1"));
+	fileSystem.isDirectory("/home/dir1");
     }
 
     @Test
@@ -245,100 +251,100 @@ public class VirtualFileSystemTest {
 	    throws FileNotFoundException, FileAlreadyExistsException, InvalidArgumentException {
 	String absolutePath = "/home/f1";
 
-	fs.createTextFile(absolutePath);
+	fileSystem.createTextFile(absolutePath);
 
-	assertFalse(fs.isDirectory(absolutePath));
+	assertFalse(fileSystem.isDirectory(absolutePath));
     }
 
     @Test(expected = InvalidArgumentException.class)
-    public void removeContentFromLinesInTextFile_InvalidPathToTextFile_ThrowInvalidPathExcepiton()
-	    throws InvalidPathException, FileNotFoundException, InvalidArgumentException {
-	fs.removeContentFromTextFile("/home/dir1/f1", 1, 2);
+    public void removeContentFromLinesInTextFile_InvalidPathToTextFile_ThrowInvalidArgumentExcepiton()
+	    throws FileNotFoundException, InvalidArgumentException {
+	fileSystem.removeContentFromTextFile("/home/dir1/f1", 1, 2);
     }
 
     @Test(expected = FileNotFoundException.class)
     public void removeContentFromLinesInTextFile_FileDoesNotExists_ThrowFileNotFoundException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.removeContentFromTextFile("f1", 1, 2);
+	fileSystem.removeContentFromTextFile("f1", 1, 2);
     }
 
     @Test(expected = FileNotFoundException.class)
     public void removeContentFromLinesInTextFile_FileIsDirectory_ThrowFileNotFoundException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.removeContentFromTextFile("/home", 1, 2);
+	fileSystem.removeContentFromTextFile("/home", 1, 2);
     }
 
     @Test
     public void removeContentFromLinesInTextFile_RemoveAllContentFromTextFile_EmptyTextFile()
 	    throws FileAlreadyExistsException, FileNotFoundException, NotEnoughMemoryException,
 	    InvalidArgumentException {
-	fs.createTextFile("/home/f1");
+	fileSystem.createTextFile("/home/f1");
 	int start = 1;
 	int end = 5;
 	for (int i = start; i <= end; i++) {
-	    fs.writeToTextFile("/home/f1", i, "hello");
+	    fileSystem.writeToTextFile("/home/f1", i, "hello");
 	}
-	fs.removeContentFromTextFile("/home/f1", start, end);
-	assertEquals("", fs.getTextFileContent("/home/f1"));
+	fileSystem.removeContentFromTextFile("/home/f1", start, end);
+	assertEquals("", fileSystem.getTextFileContent("/home/f1"));
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void getWordCount_WrongPathToTextFile_ThrowInvalidArgumentException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.getWordCount("/home/dir1/f1");
+	fileSystem.getWordCount("/home/dir1/f1");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void getWordCount_TextFileDoesNotExists_ThrowFileNotFoundException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.getWordCount("/home/f1");
+	fileSystem.getWordCount("/home/f1");
     }
 
     @Test(expected = FileNotFoundException.class)
-    public void getWordCount_Directory_ThrowFileNotFound() throws FileNotFoundException, InvalidArgumentException {
-	fs.getWordCount("/home");
+    public void getWordCount_Directory_ThrowFileNotFoundException() throws FileNotFoundException, InvalidArgumentException {
+	fileSystem.getWordCount("/home");
     }
 
     @Test
     public void getWordCount_EmptyTextFile_ReturnNumberOfWordsInTextFile()
 	    throws FileAlreadyExistsException, InvalidArgumentException, FileNotFoundException {
-	fs.createTextFile("/home/f1");
-	assertEquals(0, fs.getWordCount("/home/f1"));
+	fileSystem.createTextFile("/home/f1");
+	assertEquals(0, fileSystem.getWordCount("/home/f1"));
     }
 
     @Test
     public void getWordCount_TextFile_ReturnNumberOfWordsInTextFile() throws FileAlreadyExistsException,
 	    InvalidArgumentException, FileNotFoundException, NotEnoughMemoryException {
 	String absolutePath = "/home/f1";
-	fs.createTextFile(absolutePath);
-	fs.writeToTextFile(absolutePath, 1, "Hello World");
-	assertEquals(2, fs.getWordCount(absolutePath));
+	fileSystem.createTextFile(absolutePath);
+	fileSystem.writeToTextFile(absolutePath, 1, "Hello World");
+	assertEquals(2, fileSystem.getWordCount(absolutePath));
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void getLineCount_WrongPathToTextFile_ThrowInvalidArgumentException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.getLineCount("/home/dir1/f1");
+	fileSystem.getLineCount("/home/dir1/f1");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void getLineContent_TextFileDoesNotExists_ThrowFileNotFoundException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.getLineCount("/home/f1");
+	fileSystem.getLineCount("/home/f1");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void getLineContent_FileIsDirectory_ThrowFileNotFoundException()
 	    throws FileNotFoundException, InvalidArgumentException {
-	fs.getLineCount("/home");
+	fileSystem.getLineCount("/home");
     }
 
     @Test
     public void getLineContent_TextFile_ReturnNumberOfLinesInTextFile() throws FileNotFoundException,
 	    NotEnoughMemoryException, InvalidArgumentException, FileAlreadyExistsException {
 	String absolutePath = "/home/f1";
-	fs.createTextFile(absolutePath);
-	fs.writeToTextFile(absolutePath, 5, "hello world");
-	assertEquals(5, fs.getLineCount(absolutePath));
+	fileSystem.createTextFile(absolutePath);
+	fileSystem.writeToTextFile(absolutePath, 5, "hello world");
+	assertEquals(5, fileSystem.getLineCount(absolutePath));
     }
 }
